6eb9db708d6556da8d23a9353dbd6bc0fdfae469
MatheusPatriota <mpatriota55@gmail.com>
2021-12-17 22:14:36 -0300

    user model added to project

---
+++ src/model/User.ts
@@ -0,0 +1,14 @@
+const mongo = require('../config/database');
+
+const Schema = mongo.Schema;
+
+const UserScheme = new Schema({
+    name: {type: String, required: true},
+    admin: {type: Boolean, required: true},
+    avatar: {type: String, default: 'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png'},
+    password: {type: String, required: true},
+    active: {type: Boolean, default: true},
+    created:{type: Date, default: Date.now},
+});
+
+module.exports = mongoose.model('User', UserScheme);
